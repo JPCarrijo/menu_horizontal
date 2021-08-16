1085c1ab38b1f7e97d4883da2594af8c04a24205
JOAO PAULO CARRIJO <carrijojp@gmail.com>
2021-08-16 18:53:30 -0300

    (16/08/21) Alterações Meu Horizontal
    Signed-off-by: JOAO PAULO CARRIJO <carrijojp@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import '../src/componentes/App.css';
 import React from 'react';
+import Secoes from './Secoes'
 
 function App() {
   return (
     <>
-      <header className="corpo"> 
-        <nav className="menu">
-          <ul>
-            <li><a href=".inicio"> Home </a></li>
-            <li><a href=".cursos"> Cursos </a></li>
-            <li><a href=".contatos"> Contatos </a></li>
-            <li><a href=".parceiros"> Parceiros </a></li>
-            <li><a href=".missao"> Missão </a></li>
-            <li><a href=".sobre"> Sobre </a></li>
-          </ul>
-        </nav>
-      </header>
-
-      <section className="inicio">
-        <h2> Início </h2>
-      </section>
-
-      <section className="cursos">
-        <h3> Cursos </h3>
-      </section>
-
-      <section className="contatos">
-        <h3> Contatos </h3>
-      </section>
-
-      <section className="parceiros">
-        <h3> Parceiros </h3>
-      </section>
-
-      <section className="missao">
-        <h3> Missão </h3>
-      </section>
-
-      <section className="sobre">
-        <h3> Sobre </h3>
-      </section>
+        <Secoes />
     </>
   );
 }
