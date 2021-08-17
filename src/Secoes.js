3fa6a82d0646b13ebebcfe67e42426c0490ba7b2
JOAO PAULO CARRIJO <carrijojp@gmail.com>
2021-08-17 10:28:16 -0300

    (17/08/21) Alterações CSS
    Signed-off-by: JOAO PAULO CARRIJO <carrijojp@gmail.com>

--- src/Secoes.js
+++ src/Secoes.js
@@ -7,18 +7,29 @@ export default function Secoes() {
       <div className="menu">
         <nav className="navMenu">
           <ul>
-            <li><a href="#inicio"> Home </a></li>
-            <li><a href="#cursos"> Cursos </a></li>
-            <li><a href="#contatos"> Contatos </a></li>
-            <li><a href="#parceiros"> Parceiros </a></li>
-            <li><a href="#missao"> Missão </a></li>
-            <li><a href="#sobre"> Sobre </a></li>
+            <li><a href="#inicio" className="ini"> Home </a></li>
+            <li><a href="#cursos" className="cur"> Cursos </a></li>
+            <li><a href="#contatos" className="cont"> Contatos </a></li>
+            <li><a href="#parceiros" className="parc"> Parceiros </a></li>
+            <li><a href="#missao" className="mis"> Missão </a></li>
+            <li><a href="#sobre" className="sob"> Sobre </a></li>
           </ul>
         </nav>
       </div>
 
+      {/*<section id="inicio">
+        <h2> Início </h2>
+      </section>*/}
       <section id="inicio">
         <h2> Início </h2>
+        <aside className="aside">
+          <a href="#inicio" id="inicio2">Nosso Canal</a>
+          <a href="#cursos" id="curso2">Cursos</a>
+          <a href="#contatos" id="contatos2">Contatos</a>
+          <a href="#parceiros" id="parceiros2">Parceiros</a>
+          <a href="#missao" id="missao2">Missão</a>
+          <a href="#sobre" id="sobre2">Sobre</a>
+        </aside>
       </section>
 
       <section id="cursos">
@@ -40,17 +51,24 @@ export default function Secoes() {
       <section id="sobre">
         <h3> Sobre </h3>
       </section>
+      <aside className="aside2">
+        <a className="irTopo" href=".menu"> Topo </a>
 
-      <a className="irTopo" href=".menu"> Topo </a>
+        <a className="irInicio" href="#inicio"> Início </a>
 
-      <a className="irInicio" href="#inicio"> Início </a>
+        <a className="irCursos" href="#cursos"> Cursos </a>
 
-      <a className="irCursos" href="#cursos"> Cursos </a>
+        <a className="irContatos" href="#contatos"> Contatos </a>
 
-      <a className="irContatos" href="#contatos"> Contatos </a>
+        <a className="irParceiros" href="#parceiros"> Parceiros </a>
+
+        <a className="irMissao" href="#missao"> Missão </a>
+
+        <a className="irSobre" href="#sobre"> Sobre </a>
+      </aside>
 
       <footer>
-        <h3> Rodapé </h3>
+        <h3> Desenvolvido por João Paulo - <a href="https://site.fatecfranca.edu.br/" target="_blank"> Fatec Franca </a></h3>
       </footer>
     </>
   );
